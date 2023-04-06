b8e07907670ebafefb8513a74c3b6c6935d1d47b
Utkarsh Mishra <Utkarshmishra73444@gmail.com>
2023-04-07 00:44:01 +0530

    initial

---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import useFetch from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
+import './style.scss'
+import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
+import VideosSection from './videoSection/VideoSection'
+import Similar from './carousel/Similar'
+import Recommendation from './carousel/Recommendation'
+
+const Details = () => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+
+
+  return (
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
+    </div>
+  )
+}
+
+export default Details
---
+++ src/pages/home/popular/Popular.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+ContentWrapper;
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+import useFetch from '../../../hooks/useFetch';
+import Carousel from '../../../components/carousel/Carousel';
+
+const Popular = () => {
+  const [endPoint, setEndPoint] = useState("movie");
+  const { data, loading } = useFetch(`/${endPoint}/popular`);
+
+  const onTabChange = (tab) => {
+    setEndPoint(tab === "Movies" ? "movie" : "tv");
+  };
+  return (
+    <div className='carouselSection'>
+      <ContentWrapper>
+        <span className="carouselTitle">
+          Popular
+        </span>
+        <SwitchTabs
+          data={["Movies", "TV Shows"]}
+          onTabChange={onTabChange}
+        />
+      </ContentWrapper>
+      <Carousel
+        data={data?.results}
+        loading={loading}
+        endPoint={endPoint}
+      />
+    </div>
+  )
+}
+
+export default Popular;
